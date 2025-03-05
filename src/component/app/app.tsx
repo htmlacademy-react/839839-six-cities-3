804a8d3070b05e99ede7c5e08251e52f4bda7870
Mariya Koshelenko <k.mary.srg@gmail.com>
2025-03-05 17:41:53 +0300

    делает приватным страницу избранного

--- src/component/app/app.tsx
+++ src/component/app/app.tsx
@@ -1,10 +1,11 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { AppRoute, AuthorizationStatus } from '../../const';
 import MainScreen from '../../pages/main-screen/main-screen';
 import LoginScreen from '../../pages/login-screen/login-screen';
 import FavoritesScreen from '../../pages/favorites-screen/favorites-screen';
 import OfferScreen from '../../pages/offer-screen/offer-screen';
 import NotFoundScreen from '../../pages/not-found-screen/not-found-screen';
-import { AppRoute } from '../../const';
+import PrivateRoute from '../private-route/private-route';
 
 type OffersType = {
   id: string;
@@ -25,13 +26,16 @@ function App({offersCount, offersData}: AppScreenProps): JSX.Element {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path={AppRoute.Root}>
-          <Route index element={<MainScreen offersCount={offersCount} offersData={offersData}/>}/>
-          <Route path={AppRoute.Login} element={<LoginScreen />}/>
-          <Route path={AppRoute.Favorites} element={<FavoritesScreen />}/>
-          <Route path={AppRoute.Offer} element={<OfferScreen />}/>
-          <Route path='*' element={<NotFoundScreen />}/>
-        </Route>
+        <Route path={AppRoute.Root} element={<MainScreen offersCount={offersCount} offersData={offersData}/>}/>
+        <Route path={AppRoute.Login} element={<LoginScreen />}/>
+        <Route path={AppRoute.Favorites} element={
+          <PrivateRoute authorizationStatus={AuthorizationStatus.NoAuth}>
+            <FavoritesScreen />
+          </PrivateRoute>
+        }
+        />
+        <Route path={AppRoute.Offer} element={<OfferScreen />}/>
+        <Route path='*' element={<NotFoundScreen />}/>
       </Routes>
     </BrowserRouter>
 
--- src/const.ts
+++ src/const.ts
@@ -13,7 +13,13 @@ export const DestinationCities = [
 
 export enum AppRoute {
   Root = '/',
-  Login = 'login',
-  Favorites = 'favorites',
-  Offer = 'offer/:id'
+  Login = '/login',
+  Favorites = '/favorites',
+  Offer = '/offer/:id'
+}
+
+export enum AuthorizationStatus {
+  Auth = 'AUTH',
+  NoAuth = 'NO_AUTH',
+  Unknown = 'UNKNOWN',
 }

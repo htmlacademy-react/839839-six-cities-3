ce3e01a269c4c2b5b24285211181329e88763989
Mariya Koshelenko <k.mary.srg@gmail.com>
2025-04-29 02:26:47 +0300

    удаляет предачу комментариев в приложение

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './component/app/app';
 import { Setting } from './const';
 import { offers } from './mocks/offers';
-import { comments } from './mocks/comments';
 
 
 const root = ReactDOM.createRoot(
@@ -15,7 +14,6 @@ root.render(
     <App
       offersCount={Setting.OffersCount}
       offersData={offers}
-      commentsData={comments}
     />
   </React.StrictMode>
 );

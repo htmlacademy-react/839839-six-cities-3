176e3aec53cd331b9f9eaa690e5a51bfc8fef621
Mariya Koshelenko <k.mary.srg@gmail.com>
2025-05-12 22:24:07 +0300

    добавляет хранилище

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './component/app/app';
 import { AuthorizationStatus } from './const';
 import { offers } from './mocks/offers';
+import { Provider } from 'react-redux';
+import { store } from './store';
 
 const isAuth = AuthorizationStatus.Auth;
 
@@ -12,9 +14,11 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <App
-      offersData={offers}
-      authorizationStatus={isAuth}
-    />
+    <Provider store={store}>
+      <App
+        offersData={offers}
+        authorizationStatus={isAuth}
+      />
+    </Provider>
   </React.StrictMode>
 );

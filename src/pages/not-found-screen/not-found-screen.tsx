cc3dc614121565e8eb16a213cb0c67e396ff497d
Mariya Koshelenko <k.mary.srg@gmail.com>
2025-05-20 23:05:49 +0300

    редактирует компонент NotFoundScreen

--- src/pages/not-found-screen/not-found-screen.tsx
+++ src/pages/not-found-screen/not-found-screen.tsx
@@ -1,12 +1,23 @@
 import { Link } from 'react-router-dom';
 import { AppRoute } from '../../const';
+import './not-found-screen.css';
 
 function NotFoundScreen(): JSX.Element {
   return (
-    <>
-      <h1>404 Not Found</h1>
-      <Link to={AppRoute.Root}>Hа главную страницу</Link>
-    </>
+    <div className="not-found">
+      <h1 className="not-found__title">
+        404 Not Found
+      </h1>
+      <p className="not-found__text">
+        Упс! Похоже, мы не можем найти страницу, которую вы ищете.
+      </p>
+      <Link
+        to={AppRoute.Root}
+        className="not-found__link"
+      >
+        На главную страницу
+      </Link>
+    </div>
   );
 }
 

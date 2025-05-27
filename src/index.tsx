f1bfa680365b053af1a380a7118f873a17d7f515
Mariya Koshelenko <k.mary.srg@gmail.com>
2025-05-27 22:59:07 +0300

    добавляет загрузку избранных предложений в начало приложения

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client';
 import App from './component/app/app';
 import { Provider } from 'react-redux';
 import { store } from './store';
-import { checkAuthAction, fetchOffersAction } from './store/api-actions';
+import { checkAuthAction, fetchFavoritesAction, fetchOffersAction } from './store/api-actions';
 import ErrorMessage from './component/error-message/error-message';
-
 
 store.dispatch(fetchOffersAction());
 store.dispatch(checkAuthAction());
+store.dispatch(fetchFavoritesAction());
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement

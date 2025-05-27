e9dbd7006a0c0fd29de3f5053cb204175501723f
Mariya Koshelenko <k.mary.srg@gmail.com>
2025-05-27 08:25:16 +0300

    изменяет страницу для пустого города

--- src/component/empty-city/empty-city.tsx
+++ src/component/empty-city/empty-city.tsx
@@ -1,13 +1,35 @@
+import { useSearchParams } from 'react-router-dom';
+import { DestinationCities } from '../../const';
+import MemorizedCitiesList from '../cities-list/cities-list';
+import Header from '../header/header';
+
 function EmptyCity(): JSX.Element {
+  const [searchParams] = useSearchParams();
+  const searchCityParams = searchParams.get('city') || DestinationCities[0];
   return (
-    <div className="cities__places-container cities__places-container--empty container">
-      <section className="cities__no-places">
-        <div className="cities__status-wrapper tabs__content">
-          <b className="cities__status">No places to stay available</b>
-          <p className="cities__status-description">We could not find any property available at the moment in Dusseldorf</p>
+    <div className="page page--gray page--main">
+      <Header />
+      <main className="page__main page__main--index page__main--index-empty">
+        <h1 className="visually-hidden">Cities</h1>
+        <div className="tabs">
+          <section className="locations container">
+            <MemorizedCitiesList />
+          </section>
         </div>
-      </section>
-      <div className="cities__right-section"></div>
+        <div className="cities">
+          <div className="cities__places-container cities__places-container--empty container">
+            <section className="cities__no-places">
+              <div className="cities__status-wrapper tabs__content">
+                <b className="cities__status">No places to stay available</b>
+                <p className="cities__status-description">
+                  We could not find any property available at the moment in {searchCityParams}
+                </p>
+              </div>
+            </section>
+            <div className="cities__right-section" />
+          </div>
+        </div>
+      </main>
     </div>
   );
 }

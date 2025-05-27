578109e0372a12240ce504a7f756ea06514ae44d
Mariya Koshelenko <k.mary.srg@gmail.com>
2025-05-27 22:54:24 +0300

    изменяет тип OfferByIdType

--- src/types/offer-by-id.ts
+++ src/types/offer-by-id.ts
@@ -1,4 +1,4 @@
-import { CityType, LocationType } from './offers';
+import { OfferType } from './offers';
 
 export type User = {
   name: string;
@@ -6,16 +6,7 @@ export type User = {
   isPro: boolean;
 };
 
-export type OfferByIdType = {
-  id: string;
-  title: string;
-  type: string;
-  price: number;
-  city: CityType;
-  location: LocationType;
-  isFavorite: boolean;
-  isPremium: boolean;
-  rating: number;
+export type OfferByIdType = OfferType & {
   description: string;
   bedrooms: number;
   goods: string[];

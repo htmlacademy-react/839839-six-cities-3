9e0a6de1159f52814d95bedff9483b6212d1290a
Mariya Koshelenko <k.mary.srg@gmail.com>
2025-05-28 04:34:44 +0300

    оптимизирует отрисовку избранных предложений

--- src/component/favorites-list/favorites-list.tsx
+++ src/component/favorites-list/favorites-list.tsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import { AppRoute, DestinationCities } from '../../const';
+import { AppRoute } from '../../const';
 import { OffersType } from '../../types/offers';
 import { useAppDispatch, useAppSelector } from '../../hooks';
 import { getFavorites } from '../../store/data-precess/selectors';
@@ -23,32 +23,34 @@ function FavoritesPlaces({offersData, city}: FavoritesLocationsItemsProps) {
 
 function FavoritesLocationsItems({offersData, city}: FavoritesLocationsItemsProps) {
   const dispatch = useAppDispatch();
-  const isOffer = offersData.find((offer) => offer.city.name === city);
+  const cityOffers = offersData.filter((offer) => offer.city.name === city);
 
   const handleCityClick = (cityName: string) => () => {
     dispatch(selectCity(cityName));
   };
 
-  if (isOffer) {
-    return (
-      <li className="favorites__locations-items" key={city}>
-        <div className="favorites__locations locations locations--current">
-          <div className="locations__item">
-            <Link
-              className="locations__item-link"
-              to={AppRoute.Root}
-              onClick={handleCityClick(city)}
-            >
-              <span>{city}</span>
-            </Link>
-          </div>
-        </div>
-        <div className="favorites__places">
-          <FavoritesPlaces offersData={offersData} city={city}/>
-        </div>
-      </li>
-    );
+  if (cityOffers.length === 0) {
+    return null;
   }
+
+  return (
+    <li className="favorites__locations-items" key={city}>
+      <div className="favorites__locations locations locations--current">
+        <div className="locations__item">
+          <Link
+            className="locations__item-link"
+            to={AppRoute.Root}
+            onClick={handleCityClick(city)}
+          >
+            <span>{city}</span>
+          </Link>
+        </div>
+      </div>
+      <div className="favorites__places">
+        <FavoritesPlaces offersData={offersData} city={city}/>
+      </div>
+    </li>
+  );
 }
 
 function FavoritesList(): JSX.Element | null {
@@ -58,11 +60,15 @@ function FavoritesList(): JSX.Element | null {
     return null;
   }
 
+  const citiesWithFavorites = Array.from(
+    new Set(favoriteOffers.map((offer) => offer.city.name))
+  );
+
   return (
     <>
       <h1 className="favorites__title">Saved listing</h1>
       <ul className="favorites__list">
-        {DestinationCities.map((city) => (
+        {citiesWithFavorites.map((city) => (
           <FavoritesLocationsItems
             key={city}
             offersData={favoriteOffers}
